47639373a4d9294e881ecd3dba31f7036cf30de7
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-07-27 06:03:26 -0700

    navbar

--- src/components/employee/navbar/navbar.jsx
+++ src/components/employee/navbar/navbar.jsx
@@ -1,51 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row, Button, Badge, Space, Dropdown, Menu } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  DashboardOutlined,
+  ProjectOutlined,
+  PayCircleOutlined,
+  MessageOutlined,
+} from "@ant-design/icons";
 
 const menu = (
-	<Menu>
-		<Menu.Item key="1" icon={<UserOutlined />}>
-			1st menu item
-		</Menu.Item>
-		<Menu.Item key="2" icon={<UserOutlined />}>
-			2nd menu item
-		</Menu.Item>
-		<Menu.Item key="3" icon={<UserOutlined />}>
-			3rd item
-		</Menu.Item>
-	</Menu>
+  <Menu>
+    <Menu.Item key="1" icon={<UserOutlined />}>
+      1st menu item
+    </Menu.Item>
+    <Menu.Item key="2" icon={<UserOutlined />}>
+      2nd menu item
+    </Menu.Item>
+    <Menu.Item key="3" icon={<UserOutlined />}>
+      3rd item
+    </Menu.Item>
+  </Menu>
 );
 
 export default function NavBar(props) {
-	return (
-		<>
-			<Col offset={1} span={15}>
-				<Space align="start" size="large">
-					<Badge count={1}>
-						<Button type="primary">Button</Button>
-					</Badge>
-					<Badge>
-						<Button type="primary">Button</Button>
-					</Badge>
-					<Badge>
-						<Button type="primary">Button</Button>
-					</Badge>
-					<Badge>
-						<Button type="primary">Button</Button>
-					</Badge>
-				</Space>
-			</Col>
-			<Col span={7}>
-				<Row justify="end">
-					<Dropdown.Button
-						overlay={menu}
-						placement="bottomCenter"
-						icon={<UserOutlined />}
-					>
-						Dropdown
-					</Dropdown.Button>
-				</Row>
-			</Col>
-		</>
-	);
+  let [current, setCurrent] = useState("dashboard");
+  console.log(current);
+
+  return (
+    <>
+      <Col offset={1} span={15}>
+        <Menu
+          style={{ backgroundColor: "#f2f2f0" }}
+          selectedKeys={current}
+          mode="horizontal"
+        >
+          <Menu.Item
+            key="dashboard"
+            icon={<DashboardOutlined style={{ fontSize: 24 }} />}
+            onClick={(e) => setCurrent(e.key)}
+          >
+            Dashboard
+          </Menu.Item>
+          <Menu.Item
+            key="attendance_&_salary"
+            icon={<PayCircleOutlined style={{ fontSize: 24 }} />}
+            onClick={(e) => setCurrent(e.key)}
+          >
+            Attendance and Salary
+          </Menu.Item>
+          <Menu.Item
+            key="projects"
+            icon={<ProjectOutlined style={{ fontSize: 24 }} />}
+            onClick={(e) => setCurrent(e.key)}
+          >
+            Projects
+          </Menu.Item>
+          <Menu.Item
+            key="contact_admin"
+            icon={<MessageOutlined style={{ fontSize: 24 }} />}
+            onClick={(e) => setCurrent(e.key)}
+          >
+            Contact Admins
+          </Menu.Item>
+        </Menu>
+      </Col>
+      <Col span={7}>
+        <Row justify="end">
+          <Dropdown.Button
+            overlay={menu}
+            placement="bottomCenter"
+            icon={<UserOutlined />}
+          >
+            Dropdown
+          </Dropdown.Button>
+        </Row>
+      </Col>
+    </>
+  );
 }
--- src/components/employee/sidebar/broadcast-msg.jsx
+++ src/components/employee/sidebar/broadcast-msg.jsx
@@ -4,48 +4,48 @@ import moment from "moment";
 import { NotificationTwoTone, ScheduleTwoTone } from "@ant-design/icons";
 
 function BroadcastMsg(props) {
-	//props -> eventdate, msg, type, maybe createdate
-	const type = props.type;
-	var icon, showEvenDate;
+  //props -> eventdate, msg, type, maybe createdate
+  const type = props.type;
+  var icon, showEvenDate;
 
-	if (type === "Broadcast") {
-		icon = <NotificationTwoTone style={{ marginTop: 15, fontSize: 24 }} />;
-	} else {
-		icon = <ScheduleTwoTone style={{ marginTop: 15, fontSize: 24 }} />;
-		showEvenDate = [
-			<Tooltip title="Event Date">
-				<Badge color="cyan" text={props.eventDate} />
-			</Tooltip>,
-		];
-	}
+  if (type === "Broadcast") {
+    icon = <NotificationTwoTone style={{ marginTop: 15, fontSize: 18 }} />;
+  } else {
+    icon = <ScheduleTwoTone style={{ marginTop: 15, fontSize: 18 }} />;
+    showEvenDate = [
+      <Tooltip title="Event Date">
+        <Badge color="cyan" text={props.eventDate} />
+      </Tooltip>,
+    ];
+  }
 
-	const config = {
-		maskClosable: true,
-		title: props.type,
-		content: (
-			<>
-				<p>{props.msg}</p>
-			</>
-		),
-		okText: "Got it !",
-	};
+  const config = {
+    maskClosable: true,
+    title: props.type,
+    content: (
+      <>
+        <p>{props.msg}</p>
+      </>
+    ),
+    okText: "Got it !",
+  };
 
-	return (
-		<Comment
-			className="hovernow"
-			actions={showEvenDate}
-			avatar={icon}
-			content={<span className="broadcast-msg">{props.msg}</span>}
-			datetime={
-				<Tooltip title="Created time">
-					<span>{moment().subtract(1, "days").fromNow()}</span>
-				</Tooltip>
-			}
-			onClick={() => {
-				Modal.info(config);
-			}}
-		/>
-	);
+  return (
+    <Comment
+      className="hovernow"
+      actions={showEvenDate}
+      avatar={icon}
+      content={<span className="broadcast-msg">{props.msg}</span>}
+      datetime={
+        <Tooltip title="Created time">
+          <span>{moment().subtract(1, "days").fromNow()}</span>
+        </Tooltip>
+      }
+      onClick={() => {
+        Modal.info(config);
+      }}
+    />
+  );
 }
 
 export default BroadcastMsg;
