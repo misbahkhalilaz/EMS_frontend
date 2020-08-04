908f01090653589cf5cb753c17ceeabfd83c1ceb
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-08-05 01:14:49 +0500

    'login'

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,12 +9,19 @@ import "../components/main-theme.css";
 function App(props) {
   const [cookies, setCookie, removeCookie] = useCookies(["session"]);
 
+  if (cookies.session)
+    if (props.role === "department")
+      return <h1 onClick={() => removeCookie("session")}>logged in admin</h1>;
+    else if (props.role === "employee")
+      return (
+        <h1 onClick={() => removeCookie("session")}>logged in employee</h1>
+      );
   return <LoginForm cookies={cookies} setCookie={setCookie} />;
 }
 
 const mapStateToProps = (state, ownProps) => ({
   error: state.error,
-  state,
+  role: state.user.role,
 });
 
 export default connect(mapStateToProps, { gotError })(App);
