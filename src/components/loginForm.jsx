e31b13cc893c4457cdcdd9ee363d57940d1b34b2
Misbah Khalil <misbah.khalil.az@gmail.com>
2020-08-04 21:33:37 +0500

    login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,97 +1,187 @@
 import React, { useState } from "react";
-import { Form, Button, Col, Row, Input, Card } from "antd";
+import img from "./loginpage.png";
+import { Row, Col, Typography, Input, Button, Divider } from "antd";
 import { connect } from "react-redux";
 import { changeIsFetching, gotUser, gotError } from "../redux/actionCreators";
 
-const layout = {
-	labelCol: {
-		span: 8,
-	},
-	wrapperCol: {
-		span: 16,
-	},
-};
-const tailLayout = {
-	wrapperCol: {
-		offset: 12,
-		span: 16,
-	},
-};
-const cardLayout = {
-	bordered: true,
-	hoverable: "true",
-};
+const { Title } = Typography;
 
 function LoginForm(props) {
-	const [id, setId] = useState("");
-	const [pass, setPass] = useState("");
-	const loginApi = (credentials) => {
-		const requestOptions = {
-			method: "GET",
-			headers: {
-				role: "owner",
-				Authorization: "Basic " + btoa(credentials),
-				"Content-Type": "application/json",
-			},
-			redirect: "follow",
-		};
-		fetch("http://localhost:4000/", requestOptions)
-			.then((response) => response.text())
-			.then((res) => JSON.parse(res))
-			.then((result) => {
-				console.log(result.user);
-				props.gotUser(result.user);
-				props.setCookie("session", result.token, { path: "/" });
-				// console.log(props.user);
-			})
-			.catch((error) => {
-				props.gotError(JSON.stringify(error));
-			});
-	};
-	return (
-		<Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
-			<Col style={{ width: "420px" }}>
-				<Card title="Login:" {...cardLayout}>
-					<Form {...layout}>
-						<Form.Item
-							label="UserID:"
-							rules={[{ message: "Enter your UserID." }]}
-						>
-							<Input onChange={(e) => setId(e.target.value)} />
-						</Form.Item>
-						<Form.Item
-							label="Password:"
-							rules={[{ message: "Enter your password" }]}
-						>
-							<Input.Password onChange={(e) => setPass(e.target.value)} />
-						</Form.Item>
-						<Form.Item {...tailLayout}>
-							<Button
-								type="primary"
-								htmlType="submit"
-								onClick={() => loginApi(id + ":" + pass)}
-							>
-								Login
-							</Button>
-						</Form.Item>
-					</Form>
-				</Card>
-			</Col>
-		</Row>
-	);
+  const [id, setId] = useState("");
+  const [pass, setPass] = useState("");
+  const loginApi = (credentials, role) => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        role: role,
+        Authorization: "Basic " + btoa(credentials),
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+    };
+    fetch("http://localhost:4000/", requestOptions)
+      .then((response) => response.text())
+      .then((res) => JSON.parse(res))
+      .then((result) => {
+        console.log(result.user);
+        props.gotUser(result.user);
+        props.setCookie("session", result.token, { path: "/" });
+        // console.log(props.user);
+      })
+      .catch((error) => {
+        props.gotError(JSON.stringify(error));
+      });
+  };
+  return (
+    <>
+      <Row justify="space-around" align="middle" style={{ minHeight: "100vh" }}>
+        <Col span={17}>
+          <img src={img} width="100%" height="100%"></img>
+        </Col>
+        <Col span={6} pull={1} style={{ textAlign: "center" }}>
+          <Title level={3} className="basic-title-color">
+            Welcome to EMS
+          </Title>
+          <div
+            style={{
+              width: 250,
+              margin: "0 auto",
+              padding: 20,
+              boxShadow:
+                "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              borderRadius: 20,
+              textAlign: "center",
+            }}
+          >
+            <label style={{ float: "left" }}>UserID</label>
+            <Input
+              className="form-items"
+              style={{ marginBottom: 10 }}
+              onChange={(e) => setId(e.target.value)}
+            />
+            <label style={{ float: "left" }}>Password</label>
+            <Input
+              type="password"
+              className="form-items"
+              style={{ marginBottom: 20 }}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <Button
+              type="primary"
+              style={{ borderRadius: 10 }}
+              onClick={() => loginApi(id + ":" + pass, "employee")}
+            >
+              Signin
+            </Button>
+            <Divider>or</Divider>
+            <Button
+              type="primary"
+              style={{ borderRadius: 10 }}
+              onClick={() => loginApi(id + ":" + pass, "department")}
+            >
+              Admin Signin
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
+// import React, { useState } from "react";
+// import { Form, Button, Col, Row, Input, Card } from "antd";
+// import { connect } from "react-redux";
+// import { changeIsFetching, gotUser, gotError } from "../redux/actionCreators";
+
+// const layout = {
+// 	labelCol: {
+// 		span: 8,
+// 	},
+// 	wrapperCol: {
+// 		span: 16,
+// 	},
+// };
+// const tailLayout = {
+// 	wrapperCol: {
+// 		offset: 12,
+// 		span: 16,
+// 	},
+// };
+// const cardLayout = {
+// 	bordered: true,
+// 	hoverable: "true",
+// };
+
+// function LoginForm(props) {
+// 	const [id, setId] = useState("");
+// 	const [pass, setPass] = useState("");
+// 	const loginApi = (credentials) => {
+// 		const requestOptions = {
+// 			method: "GET",
+// 			headers: {
+// 				role: "owner",
+// 				Authorization: "Basic " + btoa(credentials),
+// 				"Content-Type": "application/json",
+// 			},
+// 			redirect: "follow",
+// 		};
+// 		fetch("http://localhost:4000/", requestOptions)
+// 			.then((response) => response.text())
+// 			.then((res) => JSON.parse(res))
+// 			.then((result) => {
+// 				console.log(result.user);
+// 				props.gotUser(result.user);
+// 				props.setCookie("session", result.token, { path: "/" });
+// 				// console.log(props.user);
+// 			})
+// 			.catch((error) => {
+// 				props.gotError(JSON.stringify(error));
+// 			});
+// 	};
+// 	return (
+// 		<Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
+// 			<Col style={{ width: "420px" }}>
+// 				<Card title="Login:" {...cardLayout}>
+// 					<Form {...layout}>
+// 						<Form.Item
+// 							label="UserID:"
+// 							rules={[{ message: "Enter your UserID." }]}
+// 						>
+// 							<Input onChange={(e) => setId(e.target.value)} />
+// 						</Form.Item>
+// 						<Form.Item
+// 							label="Password:"
+// 							rules={[{ message: "Enter your password" }]}
+// 						>
+// 							<Input.Password onChange={(e) => setPass(e.target.value)} />
+// 						</Form.Item>
+// 						<Form.Item {...tailLayout}>
+// 							<Button
+// 								type="primary"
+// 								htmlType="submit"
+// 								onClick={() => loginApi(id + ":" + pass)}
+// 							>
+// 								Login
+// 							</Button>
+// 						</Form.Item>
+// 					</Form>
+// 				</Card>
+// 			</Col>
+// 		</Row>
+// 	);
+// }
+
 const mapStateToProps = (state, ownProps) => ({
-	cookies: ownProps.cookies,
-	isFetching: state.isFetching,
-	user: state.user,
-	setCookie: ownProps.setCookie,
-	error: state.error,
-	state,
+  cookies: ownProps.cookies,
+  isFetching: state.isFetching,
+  user: state.user,
+  setCookie: ownProps.setCookie,
+  error: state.error,
+  state,
 });
 
 export default connect(mapStateToProps, {
-	changeIsFetching,
-	gotUser,
-	gotError,
+  changeIsFetching,
+  gotUser,
+  gotError,
 })(LoginForm);
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import LoginForm from "../components/loginForm";
+import { useCookies } from "react-cookie";
+import { connect } from "react-redux";
+import { gotError } from "../redux/actionCreators";
+import "antd/dist/antd.css";
+import "../components/main-theme.css";
 
-import Employee from "./employee";
+function App(props) {
+  const [cookies, setCookie, removeCookie] = useCookies(["session"]);
 
-function App() {
-  return <Employee />;
+  return <LoginForm cookies={cookies} setCookie={setCookie} />;
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => ({
+  error: state.error,
+  state,
+});
+
+export default connect(mapStateToProps, { gotError })(App);
